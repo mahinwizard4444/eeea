3dfdd3d78920e1a8de8d2cc4b28e8e341fb04939
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-04-04 22:01:17 +0530

    Delete plugins/Rename directory

--- plugins/Rename/bot/__init__.py
+++
@@ -1,7 +0,0 @@
-# (c) @Aadhi000
-
-from dotenv import load_dotenv
-import bot.client
-
-load_dotenv()
-bot = bot.client.Client()
--- plugins/Rename/bot/__main__.py
+++
@@ -1,7 +0,0 @@
-# (c) @Aadhi000
-
-import bot
-
-
-if __name__ == "__main__":
-    bot.bot.run()
