c6c72b0ca20dd129cf91908d9d49cc360caa1e55
mahinwizard4444 <86041088+mahinwizard4444@users.noreply.github.com>
2022-04-25 10:59:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /eeea
 fi
-cd /Film-Club
+cd /eeea
 pip3 install -U -r requirements.txt
 echo "Starting Bot....👀"
 python3 bot.py
